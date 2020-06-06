5e7584458693b674c03d30c5ac8a357eb5509798
mattchupp <mattlchupp@gmail.com>
2020-06-05 22:40:46 -0500

    styling layout

--- src/components/HistoryCard.js
+++ src/components/HistoryCard.js
@@ -4,30 +4,44 @@ import moment from 'moment';
 
 
 /* Styles */
+const Title = styled.h2`
+  font-weight: bold; 
+`
+
 const Card = styled.div`
-  background-color: #0e0e0e;
+  background-color: #030200;
   padding: 1rem; 
   margin: 2rem 0;
   border-radius: 10px; 
 `
 const Details = styled.p`
-  margin: 1rem 0 0 0;
+  margin: 1.5rem 0;
   font-size: 1rem;
   line-height: 1.5rem;
+`
+const Reading = styled.ul`
+  list-style-type: none; 
+`
+const ReadingItem = styled.li`
+  display: inline;
+  margin-right: 1rem;
 `
 
 
 function HistoryCard(props) {
 
-  return(
+  return (
     <Card key={props.id}>
-      <h2>{props.title}</h2>
+      <Title>{props.title}</Title>
       <span>{moment.unix(props.date).format("LLLL")}</span>
       <Details>{props.details}</Details>
-      <p>More Reading</p>
-      <a href={props.reddit}>Reddit</a>
-      <a href={props.article}>Article</a>
-      <a href={props.wikipedia}>Wikipedia</a>
+      <Reading>
+        <ReadingItem>More Reading:</ReadingItem>
+        <ReadingItem><a href={props.reddit}>Reddit</a></ReadingItem>
+        <ReadingItem><a href={props.article}>Article</a></ReadingItem>
+        <ReadingItem><a href={props.wikipedia}>Wikipedia</a></ReadingItem>
+      </Reading>
+      
     </Card>
   )
 
