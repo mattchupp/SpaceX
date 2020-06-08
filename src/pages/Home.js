7f5872e8ffbb09fbd684375151c406a2c727b908
mattchupp <mattlchupp@gmail.com>
2020-06-07 23:14:11 -0500

    displayed date of next launch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@
 import React from 'react'; 
 import LatestLaunch from '../components/LatestLaunch';
 import NextLaunch from '../components/NextLaunch'; 
-import dm2Launch from '../images/dm2_launch.jpg'; 
+import Countdown from '../components/LaunchCountdown';
+// import dm2Launch from '../images/dm2_launch.jpg'; 
 // import styled from 'styled-components'; 
 import '../App.css';
 
@@ -18,8 +19,8 @@ function Home() {
   return (
     <div>
       <div className="splash-container">
-        <img src={dm2Launch} style={img} alt="spacex launch"/>
-        
+        {/* <img src={dm2Launch} style={img} alt="spacex launch"/> */}
+        <Countdown />
       </div>
       <div className="container">
         <LatestLaunch />
