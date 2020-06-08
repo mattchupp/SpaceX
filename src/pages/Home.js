dccc3bc7d896a1a91e742451b7993313caa1f002
mattchupp <mattlchupp@gmail.com>
2020-06-07 22:28:16 -0500

    image with centered text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import LatestLaunch from '../components/LatestLaunch';
 import NextLaunch from '../components/NextLaunch'; 
 import dm2Launch from '../images/dm2_launch.jpg'; 
 import styled from 'styled-components'; 
+import '../App.css';
 // import '../App.css'
 
 const img = {
@@ -13,26 +14,17 @@ const img = {
   'position': 'relative'
 }
 
-const SplashContainer = styled.div`
-  width: 100%,
-  height: 100%
-  position: relative
-`
-
-const SplashHeader = styled.h1`
-  position: absolute,
-  top: 50%,
-  left: 50%
-`
 
 function Home() {
 
   return (
     <div>
-      <SplashContainer>
+      <div className="splash-container">
         <img src={dm2Launch} style={img} alt="spacex launch"/>
-        <h1>Text</h1>
-      </SplashContainer>
+        <div className="splash-container-wrapper">
+         <h1>Text</h1>
+        </div>
+      </div>
       <div className="container">
         <LatestLaunch />
         <NextLaunch />
