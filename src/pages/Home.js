178c06cc9722744d8b7b3bd0a21cf83aafcf218b
mattchupp <mattlchupp@gmail.com>
2020-06-07 21:43:46 -0500

    added splash image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,19 @@
 import React from 'react'; 
 import LatestLaunch from '../components/LatestLaunch';
 import NextLaunch from '../components/NextLaunch'; 
+import dm2Launch from '../images/dm2_launch.jpg'; 
 // import '../App.css'
+
+const img = {
+  'width': '100%', 
+  'height': '80vh'
+}
 
 function Home() {
 
   return (
     <div>
+      <img src={dm2Launch} style={img} alt="spacex launch"/>
       <div className="container">
         <LatestLaunch />
         <NextLaunch />
