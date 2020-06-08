c7cf6667e97ed5310814da24ba2a3992666cfd99
mattchupp <mattlchupp@gmail.com>
2020-06-07 22:14:57 -0500

    changing styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,35 @@ import React from 'react';
 import LatestLaunch from '../components/LatestLaunch';
 import NextLaunch from '../components/NextLaunch'; 
 import dm2Launch from '../images/dm2_launch.jpg'; 
+import styled from 'styled-components'; 
 // import '../App.css'
 
 const img = {
   'width': '100%', 
-  'height': '80vh'
+  'height': '100%',
+  'position': 'relative'
 }
+
+const SplashContainer = styled.div`
+  width: 100%,
+  height: 100%
+  position: relative
+`
+
+const SplashHeader = styled.h1`
+  position: absolute,
+  top: 50%,
+  left: 50%
+`
 
 function Home() {
 
   return (
     <div>
-      <img src={dm2Launch} style={img} alt="spacex launch"/>
+      <SplashContainer>
+        <img src={dm2Launch} style={img} alt="spacex launch"/>
+        <h1>Text</h1>
+      </SplashContainer>
       <div className="container">
         <LatestLaunch />
         <NextLaunch />
