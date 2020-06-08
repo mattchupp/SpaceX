bf6fe92db3b24c85965b0c054fad076ea9e0b665
mattchupp <mattlchupp@gmail.com>
2020-06-07 23:12:29 -0500

    just get date of next launch -- turn to countdown eventually

---
+++ src/components/LaunchCountdown.js
@@ -0,0 +1,42 @@
+/* Display latest launch from spacex */
+import React, { useEffect, useState } from 'react'; 
+import axios from 'axios'; 
+import moment from 'moment';
+import styled from 'styled-components'; 
+
+
+const Date = styled.p`
+  font-size: 3rem;
+`
+ 
+
+function Countdown() {
+
+  const [nextLaunch, setNextLaunch] = useState([]); 
+  
+  useEffect(() => {
+
+    axios.get('https://api.spacexdata.com/v3/launches/next')
+    .then(response => {
+      console.log(response.data)
+      setNextLaunch(response.data)
+    })
+    .then(error => {
+      console.log(error)
+    })
+    
+  }, [])
+
+  return (
+    <>
+      <h1>Next Launch</h1>
+      <Date>{moment.unix(nextLaunch.launch_date_unix).format("LL @ h:mm a")} </Date>
+    </>
+  )
+}
+
+
+export default Countdown
+
+
+
