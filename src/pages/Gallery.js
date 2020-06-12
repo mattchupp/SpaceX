adcb46b2601516608b1c48900e8e03656ceaccc5
mattchupp <mattlchupp@gmail.com>
2020-06-12 15:49:19 -0500

    show all photos from launches

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,27 +1,14 @@
 /* Display upcoming launches from spacex */
 import React, { useEffect, useState } from 'react'; 
 import axios from 'axios'; 
-import styled from 'styled-components'; 
-import Card from '../components/Card'; 
+// import styled from 'styled-components'; 
+// import Card from '../components/Card'; 
 
-
-const Title = styled.h1`
-  font-weight: bold; 
-  font-size: 2.6rem;
-  margin-bottom: 1rem;
-  margin-top: 2rem;
-`
-
-const Search = styled.input`
-  font-size: 1rem;
-  padding: .5rem .25rem;
-`
 
 
 function Gallery() {
 
   const [pastLaunches, setPastLaunches] = useState([]); 
-  const [filterBy, setFilterBy] = useState('');
   
   
   useEffect(() => {
@@ -30,6 +17,7 @@ function Gallery() {
     .then(response => {
       console.log(response.data)
       setPastLaunches(response.data)
+      // setPastLaunches(response.data)
     })
     .then(error => {
       console.log(error)
@@ -37,17 +25,10 @@ function Gallery() {
     
   }, [])
 
-  let filteredLaunches = pastLaunches.filter((launch) => {
-    return launch.mission_name.toLowerCase().indexOf(filterBy.toLowerCase()) !== -1;
-  })
-  
-
   return (
     <div>
-      {filteredLaunches.map((event) => (
-        <Card 
-          images={event.links.flickr_images}
-        />
+      {pastLaunches.map((launch) => (
+        <img key={launch.mission_name} src={launch.links.flickr_images} alt={launch.mission_name} />
       ))}
     </div>
   )
