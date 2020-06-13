c07a0a1defa46528da83e08a9c04b7b4b1a252e5
mattchupp <mattlchupp@gmail.com>
2020-06-12 21:39:38 -0500

    if image array is empty ? null : return image

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,14 +1,18 @@
 /* Display upcoming launches from spacex */
 import React, { useEffect, useState } from 'react'; 
 import axios from 'axios'; 
-// import styled from 'styled-components'; 
+import styled from 'styled-components'; 
 // import Card from '../components/Card'; 
 
-
+const ImgContainer = styled.div`
+  margin: .5rem 0; 
+  border: 1px solid red; 
+`
 
 function Gallery() {
 
   const [pastLaunches, setPastLaunches] = useState([]); 
+  const [isLoaded, setIsLoaded] = useState(false)
   
   
   useEffect(() => {
@@ -17,7 +21,7 @@ function Gallery() {
     .then(response => {
       console.log(response.data)
       setPastLaunches(response.data)
-      // setPastLaunches(response.data)
+      setIsLoaded(true)
     })
     .then(error => {
       console.log(error)
@@ -25,13 +29,30 @@ function Gallery() {
     
   }, [])
 
-  return (
-    <div>
-      {pastLaunches.map((launch) => (
-        <img key={launch.mission_name} src={launch.links.flickr_images} alt=""/>
-      ))}
-    </div>
-  )
+  const border = {
+    'border': '1px solid white'
+  }
+
+  if(isLoaded) {
+    return (
+      <div style={border}>
+        {pastLaunches.map((launch) => (
+          <>
+          {launch.links.flickr_images == '' ? null : 
+            <ImgContainer>
+              <img key={launch.mission_name} src={launch.links.flickr_images} alt=""/>
+            </ImgContainer>
+          }
+          </>
+        ))}
+      </div>
+    )
+  } else {
+    return (
+      <div><p>Loading...</p></div>
+    )
+  }
+  
 }
 
 
